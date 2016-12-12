130bf251ef33b8278bb3f6f70c42eb5250b96760
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-12 13:31:35 -0500

    first commit

---
+++ Release/spispectrumplay.bat
@@ -0,0 +1,5 @@
+rem start spispectrumplay.exe testwav.wav 10 200 200 0 "audiospispectrumclass" "audiospispectrum title" "begin - copy.ahk" "end - copy.ahk" 1 100
+rem start spispectrumplay.exe "D:\oifii-org\httpdocs\ha-org\had\dj-oifii\house_wav\dj-oifii_heavyhouse(Shivers-Armin van Buuren)_007.wav" 3600 1 1 0 "audiospispectrumclass" "audiospispectrum title" "begin - copy.ahk" "end - copy.ahk" 1 100 1920 1080 150
+rem start spispectrumplay.exe testwav.wav -1 0 0 4 "audiospispectrumclass" "audiospispectrum title" "" "" 1 100 1920 1080 150
+rem start spispectrumplay.exe "C:\Users\do-org\Downloads\dj-oifii_heavyhouse(Shivers-Armin van Buuren)_007.mp3" -1 0 0 4 "audiospispectrumclass" "audiospispectrum title" "" "" 1 100 1920 1080 150
+start spispectrumplay.exe "C:\Users\do-org\Downloads\dj-oifii_trance-euphoric-uplifting_2015feb06.wav" -1 0 0 4 "audiospispectrumclass" "audiospispectrum title" "" "" 5 100 1920 1080 150
